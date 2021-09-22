4f3e033e1dd6e78136d99444c2aecfbb5c0b3fcd
ashikka <ashikagupta28@gmail.com>
2021-09-22 15:14:43 +0530

    feat: Improved LandingPage UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
-import './App.scss';
+import React from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { Container, Button } from "react-bootstrap";
+import "./App.scss";
+import LandingPage from "./pages/LandingPage/LandingPage";
 
 function App() {
   return (
+    <>
+    <div className="d-flex-row justify-content-around p-4 m-0 navbar">
+    <a href="/">
+      {/* <img src={Logo} alt="logo" className="logo" /> */}
+    </a>
+    <a href="#distros">
+      <Button>Test me</Button>
+    </a>
+  </div>
     <Router basename={process.env.PUBLIC_URL}>
       <Container fuild className="App">
         <Switch>
           <Route exact path="/">
-            <h1>Hello world</h1>
+            <LandingPage />
           </Route>
         </Switch>
       </Container>
     </Router>
+    </>
   );
 }
 
--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './LandingPage.scss';
 import { Row } from 'react-bootstrap';
 import Tilt from 'react-tilt';
+import ArchLinux from '../../assets/arch.png';
 
 const LandingPage = () => (
   <>
@@ -11,39 +12,33 @@ const LandingPage = () => (
           <div>
             <div className="w-50" />
             <h1 className="header">
-              Run and test Linux
-              <br />
-              in your browser
+              Golf it!
             </h1>
           </div>
           <br />
           <div className="text-center">
             <h5 className="subheader">
-              No installations, no long waiting times, its an
+            Golf it! is a game designed to let
               <br />
-              operating system in your browser
+              you show off your code-fu
             </h5>
             <br />
             <p className="paragraph">
-              With
-              {' '}
-              {' '}
-              <b>linucks.io</b>
-              {' '}
-              you can yuck out all the bad distros
+            by solving problems in 
+              <br></br>
+              the least number of <b>characters.</b>
             </p>
           </div>
         </div>
       </div>
     </Row>
     <Row className="d-flex parent justify-content-center">
-      {/* <Tilt className="Tilt" options={{ max: 25, easing: 'cubic-bezier(.17,.67,.74,.76)' }}>
+      <Tilt className="Tilt" options={{ max: 25, easing: 'cubic-bezier(.17,.67,.74,.76)' }}>
         <div className="parent-div h-100 w-100">
           <div className="glow" />
           <img src={ArchLinux} alt="" className="arch" />
         </div>
-
-      </Tilt> */}
+      </Tilt>
 
     </Row>
 
