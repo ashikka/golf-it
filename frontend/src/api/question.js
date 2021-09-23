b697724763dd1e921ebc2214b9841abc907f48fa
ashikka <ashikagupta28@gmail.com>
2021-09-23 12:54:01 +0530

    feat: Added Coding page

---
+++ frontend/src/api/question.js
@@ -0,0 +1,33 @@
+const res = {
+    data: {
+      questions: [
+        {
+          questionName: "yes",
+          question: "something",
+          id: "1",
+        },
+        {
+          questionName: "yes",
+          question: "something",
+          id: "2",
+        },
+        {
+          questionName: "yes",
+          question: "something",
+          id: "3",
+        },
+        {
+          questionName: "yes",
+          question: "something",
+          id: "4",
+        },
+        {
+          questionName: "yes",
+          question: "something",
+          id: "5",
+        },
+      ],
+    },
+  };
+
+export default res;
