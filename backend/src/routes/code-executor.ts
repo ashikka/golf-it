dd3f7aa6dbf7b35f87b485920f84c010b36b0149
ashikka <ashikagupta28@gmail.com>
2021-09-23 22:27:08 +0530

    feat: Setup code-executor

---
+++ backend/src/routes/code-executor.ts
@@ -0,0 +1,17 @@
+import { CodeExecutor } from "code-executor";
+
+const codeExecutor = new CodeExecutor("myExecutor", "redis://127.0.0.1:6379");
+
+const executeCode = async (language: any, code: any, testCases: any) => {
+  const input = {
+    language,
+    code,
+    testCases,
+    timeout: 2,
+  };
+
+  const results = await codeExecutor.runCode(input);
+  return results;
+};
+
+export default executeCode;
